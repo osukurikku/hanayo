480df965625262e1bcfaa60f7a18f25c6817b908
Mike Shenin <i@kotrik.ru>
2019-08-26 17:25:33 +0300

    ough...

--- templates.go
+++ templates.go
@@ -57,7 +57,7 @@ func loadTemplates(subdir string) {
 		}
 
 		fullName := "templates" + subdir + "/" + i.Name()
-		_c := parseConfig(fullName)BaseGet
+		_c := parseConfig(fullName)
 		var c templateConfig
 		if _c != nil {
 			c = *_c
