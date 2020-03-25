c372349f6eb19380b62e632ca13220a598bf2d87
Mike Shenin <i@kotrik.ru>
2020-03-25 17:40:20 +0100

    upd: fancy user stats test fix

--- funcmap.go
+++ funcmap.go
@@ -524,7 +524,7 @@ var funcMap = template.FuncMap{
 	"getFancyUsersStats": func() (graph string) {
 		graphs := []FancyGraphSimple{}
 		
-		rows, err := db.Query("SELECT users_osu FROM bancho_stats WHERE banchostats_id mod 10 = 0 ORDER BY banchostats_id ASC LIMIT 144")
+		rows, err := db.Query("SELECT sub.users_osu FROM (SELECT banchostats_id, users_osu FROM bancho_stats WHERE banchostats_id mod 10 = 0 ORDER BY banchostats_id DESC LIMIT 144) AS sub ORDER BY sub.banchostats_id ASC")
 		if err != nil {
 			fmt.Println(err)
 			return graph
@@ -540,6 +540,7 @@ var funcMap = template.FuncMap{
 
 			graphItem.X = iter
 			iter+=1
+			//graphs = append([]FancyGraphSimple{graphItem}, graphs...)
 			graphs = append(graphs, graphItem)
 		}
 		
